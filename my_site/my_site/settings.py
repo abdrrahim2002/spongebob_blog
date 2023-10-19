bae12d671ed61476b8cb3979c0ec1eddb0db4d78
AbdRrahim <python.py2002@gmail.com>
2023-10-19 18:07:53 +0100

    try vercele method

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n#-9ja&i-07k1rj$lgrn@yk0gbcjor$8*moui(8dmf%w%j2g_6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vercel.app']
 
 
 # Application definition
